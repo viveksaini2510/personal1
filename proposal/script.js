54c4f0f366bfa0c6da69448a05264cf73fae4573
Vivek Saini <125733850+viveksaini2510@users.noreply.github.com>
2025-02-08 00:13:40 +0530

    Add files via upload

---
+++ proposal/script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert("Yay! I'm so happy you said yes! ❤️");
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert("That's okay! I love you no matter what! 💖");
+});
